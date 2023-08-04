ba0c4ba55b62d4515a2bf94882f62cad52a7b4b2
val <valentin@agence-webup.com>
2023-08-04 12:11:30 +0200

    fix(publish): correctly process namespace/resources stub

--- src/HeliumCore.php
+++ src/HeliumCore.php
@@ -9,13 +9,13 @@ class HeliumCore
     public function getDefaultStubProcessor(): Closure
     {
         return function (string $content): string {
-            $content = str_replace('{{ $namespace }} ', config('helium-core.namespace'), $content);
-            $content = str_replace('{{ $resources }} ', config('helium-core.resources'), $content);
+            $content = str_replace('{{ $namespace }}', config('helium-core.namespace'), $content);
+            $content = str_replace('{{ $resources }}', config('helium-core.resources'), $content);
 
-            $content = str_replace('{{ $as }}', config('helium-core.routing.as'), $content);
-            $content = str_replace('{{ $prefix }}', config('helium-core.routing.prefix'), $content);
+            $content = str_replace('{{ $routing.as }}', config('helium-core.routing.as'), $content);
+            $content = str_replace('{{ $routing.prefix }}', config('helium-core.routing.prefix'), $content);
             $content = str_replace(
-                '{{ $middleware }}',
+                '{{ $routing.middleware }}',
                 sprintf(
                     "['%s']",
                     implode("', '", config('helium-core.routing.middleware'))
@@ -23,10 +23,10 @@ class HeliumCore
                 $content
             );
 
-            $content = str_replace('{{ $table_name }}', config('helium-core.features.users.table_name'), $content);
-            $content = str_replace('{{ $model_name }}', config('helium-core.features.users.model_name'), $content);
-            $content = str_replace('{{ $guard_name }}', config('helium-core.features.users.guard_name'), $content);
-            $content = str_replace('{{ $controller_name }}', config('helium-core.features.users.controller_name'), $content);
+            $content = str_replace('{{ $features.users.table_name }}', config('helium-core.features.users.table_name'), $content);
+            $content = str_replace('{{ $features.users.model_name }}', config('helium-core.features.users.model_name'), $content);
+            $content = str_replace('{{ $features.users.guard_name }}', config('helium-core.features.users.guard_name'), $content);
+            $content = str_replace('{{ $features.users.controller_name }}', config('helium-core.features.users.controller_name'), $content);
 
             return $content;
         };
