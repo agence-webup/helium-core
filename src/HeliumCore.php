dd5edcf067940c8908b69acfc21345aff8aef48f
val <valentin@agence-webup.com>
2024-03-15 12:13:21 +0100

    fix(mvp): cleanup config and comment out defaultStubProcessor

--- src/HeliumCore.php
+++ src/HeliumCore.php
@@ -8,19 +8,21 @@ class HeliumCore
 {
     public function getDefaultStubProcessor(): Closure
     {
-        return function (string $content): string {
-            $content = str_replace('{{ $namespace }}', config('helium-core.namespace'), $content);
-            $content = str_replace('{{ $resources }}', config('helium-core.resources'), $content);
+        // todo(val): impl this properly after mvp.
+        // return function (string $content): string {
+        //     $content = str_replace('{{ $namespace }}', config('helium-core.namespace'), $content);
+        //     $content = str_replace('{{ $resources }}', config('helium-core.resources'), $content);
 
-            $content = str_replace('{{ $routing->as }}', config('helium-core.routing.as'), $content);
-            $content = str_replace('{{ $routing->prefix }}', config('helium-core.routing.prefix'), $content);
+        //     $content = str_replace('{{ $routing->as }}', config('helium-core.routing.as'), $content);
+        //     $content = str_replace('{{ $routing->prefix }}', config('helium-core.routing.prefix'), $content);
 
-            $content = str_replace('{{ $features.users.table_name }}', config('helium-core.features.users.table_name'), $content);
-            $content = str_replace('{{ $features.users.model_name }}', config('helium-core.features.users.model_name'), $content);
-            $content = str_replace('{{ $features.users.guard_name }}', config('helium-core.features.users.guard_name'), $content);
-            $content = str_replace('{{ $features.users.controller_name }}', config('helium-core.features.users.controller_name'), $content);
+        //     $content = str_replace('{{ $features->users->table_name }}', config('helium-core.features.users.table_name'), $content);
+        //     $content = str_replace('{{ $features->users->model_name }}', config('helium-core.features.users.model_name'), $content);
+        //     $content = str_replace('{{ $features->users->guard_name }}', config('helium-core.features.users.guard_name'), $content);
+        //     $content = str_replace('{{ $features->users->controller_name }}', config('helium-core.features.users.controller_name'), $content);
 
-            return $content;
-        };
+        //     return $content;
+        // };
+        return fn (string $content): string => $content;
     }
 }
--- src/HeliumCoreServiceProvider.php
+++ src/HeliumCoreServiceProvider.php
@@ -18,14 +18,10 @@ class HeliumCoreServiceProvider extends PackageServiceProvider
 
         $this->publishes([
             $this->package->basePath('/../resources/views/components') => base_path('resources/views/vendor/hui/components'),
-            // $this->package->basePath('/../resources/views/datatable') => base_path('resources/views/vendor/hui/datatable'),
-            // $this->package->basePath('/../resources/views/layout') => base_path('resources/views/vendor/hui/layout'),
         ], 'helium-components');
 
         $this->loadViewsFrom($this->package->basePath('/../resources/views'), 'hui');
 
-        // $filename = config('helium-core.routing.filename');
-        // $routes = base_path("routes/$filename.php");
         $routes = base_path('routes/admin.php');
 
         if (file_exists($routes)) {
