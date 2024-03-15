445221a9829ff277ec53041f8b8bca5666d3a50d
val <valentin@agence-webup.com>
2024-03-15 11:43:08 +0100

    fix(provider): enforce standard names and publish make command

--- src/HeliumCoreServiceProvider.php
+++ src/HeliumCoreServiceProvider.php
@@ -4,6 +4,7 @@ namespace Webup\HeliumCore;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Webup\HeliumCore\Commands\DatatableMakeCommand;
 use Webup\HeliumCore\Commands\Publish;
 
 class HeliumCoreServiceProvider extends PackageServiceProvider
@@ -13,18 +14,20 @@ class HeliumCoreServiceProvider extends PackageServiceProvider
         $package
             ->name('helium-core')
             ->hasConfigFile('helium-core')
-            ->hasCommand(Publish::class);
+            ->hasCommands(Publish::class, DatatableMakeCommand::class);
 
         $this->publishes([
             $this->package->basePath('/../resources/views/components') => base_path('resources/views/vendor/hui/components'),
-            $this->package->basePath('/../resources/views/datatable') => base_path('resources/views/vendor/hui/datatable'),
-            $this->package->basePath('/../resources/views/layout') => base_path('resources/views/vendor/hui/layout'),
+            // $this->package->basePath('/../resources/views/datatable') => base_path('resources/views/vendor/hui/datatable'),
+            // $this->package->basePath('/../resources/views/layout') => base_path('resources/views/vendor/hui/layout'),
         ], 'helium-components');
 
         $this->loadViewsFrom($this->package->basePath('/../resources/views'), 'hui');
 
-        $filename = config('helium-core.routing.filename');
-        $routes = base_path("routes/$filename.php");
+        // $filename = config('helium-core.routing.filename');
+        // $routes = base_path("routes/$filename.php");
+        $routes = base_path('routes/admin.php');
+
         if (file_exists($routes)) {
             $this->loadRoutesFrom($routes);
         }
